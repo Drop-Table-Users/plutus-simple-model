bf3b5541650d99dd31c9dc7ece350874749361b0
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-11-07 14:33:24 +0200

    change to liqwid-nix flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,59 @@
 {
   description = "plutus-simple-model";
+
   nixConfig = {
-    # We don't use Recursive Nix yet.
-    extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" "recursive-nix" ];
-    extra-substituters = [ "https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org" ];
-    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc=" ];
+    extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
+    extra-substituters = [ "https://cache.iog.io" "https://cache.zw3rk.com" ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
     allow-import-from-derivation = "true";
-    bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]psm \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
+    max-jobs = "auto";
+    auto-optimise-store = "true";
   };
 
   inputs = {
-    tooling.url = "github:iburzynski/mlabs-tooling.nix";
-    plutarch.url = "github:plutonomicon/plutarch-plutus";
+    nixpkgs.follows = "liqwid-nix/nixpkgs";
+    nixpkgs-latest.url = "github:NixOS/nixpkgs";
+    liqwid-nix = {
+      url = "github:Liqwid-Labs/liqwid-nix/v2.9.2";
+      inputs.nixpkgs-latest.follows = "nixpkgs-latest";
+    };
+    liqwid-libs.url =
+      "github:Liqwid-Labs/liqwid-libs";
+    cardano-base.url = "github:input-output-hk/cardano-base/cardano-strict-containers-0.1.1.0";
   };
 
-  outputs = inputs@{ self, tooling, plutarch, nixpkgs, ... }:
-    tooling.lib.mkFlake { inherit self; }
-      {
-        imports = [
-          (tooling.lib.mkHaskellFlakeModule1 {
-            project.src = ./.;
-            project.extraHackage = [
-              "${plutarch}"
-              "${plutarch}/plutarch-extra"
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.liqwid-nix.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          pkgs = import inputs.nixpkgs-latest { inherit system; };
+        in
+        {
+          # See: https://liqwid-labs.github.io/liqwid-nix/reference/modules.html
+          onchain.default = {
+            src = ./.;
+            ghc.version = "ghc925";
+            fourmolu.package = pkgs.haskellPackages.fourmolu;
+            hlint = { };
+            cabalFmt = { };
+            hasktags = { };
+            applyRefact = { };
+            shell = { };
+            hoogleImage.enable = false;
+            enableCabalFormatCheck = true;
+            enableHaskellFormatCheck = true;
+            enableBuildChecks = true;
+            extraHackageDeps = [
+              "${inputs.cardano-base}/cardano-strict-containers"
             ];
-            toHaddock = [
-#              "plutarch"
-#              "plutarch-extra"
-#              "cardano-crypto"
-#              "cardano-ledger-alonzo"
-#              "cardano-ledger-babbage"
-#              "cardano-ledger-core"
-            ];
-
-          })
-        ];
-      };
+          };
+        };
+    };
 }
